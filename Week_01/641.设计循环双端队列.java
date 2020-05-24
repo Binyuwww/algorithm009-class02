22f91ad4c3bea10279ec116f00b5be757d7aeecb
Admin <binyuw96@gmail.com>
2020-05-24 01:34:20 -0400

    MyCircularDeque

--- Week_01/641.设计循环双端队列.java
+++ Week_01/641.设计循环双端队列.java
@@ -6,110 +6,105 @@
 
 // @lc code=start
 class MyCircularDeque {
-    private int capacity;
     private int[] arr;
+    private int size;
     private int front;
-    private int rear;
+    private int last;
 
     /**
      * Initialize your data structure here. Set the size of the deque to be k.
      */
     public MyCircularDeque(int k) {
-        capacity = k + 1;
-        arr = new int[capacity];
+        if (k < 0) {
+            throw new IllegalArgumentException("Error");
+        }
 
-        // 头部指向第 1 个存放元素的位置
-        // 插入时，先减，再赋值
-        // 删除时，索引 +1（注意取模）
+        arr = new int[k];
+        size = 0;
         front = 0;
-        // 尾部指向下一个插入元素的位置
-        // 插入时，先赋值，再加
-        // 删除时，索引 -1（注意取模）
-        rear = 0;
+        last = 0;
     }
 
-    /**
-     * Adds an item at the front of Deque. Return true if the operation is successful.
-     */
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
     public boolean insertFront(int value) {
-        if (isFull()) {
+        if (size == arr.length) {
             return false;
         }
-        front = (front - 1 + capacity) % capacity;
-        arr[front] = value;
+
+        if (front == 0) {
+            arr[arr.length - 1] = value;
+            front = arr.length - 1;
+        }
+        else {
+            arr[--front] = value;
+        }
+        size ++;
         return true;
     }
-
-    /**
-     * Adds an item at the rear of Deque. Return true if the operation is successful.
-     */
+    
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
     public boolean insertLast(int value) {
-        if (isFull()) {
+        if (size == arr.length) {
             return false;
         }
-        arr[rear] = value;
-        rear = (rear + 1) % capacity;
+
+        arr[last] = value;
+        size ++;
+        last = last == arr.length - 1 ? 0 : last + 1;
         return true;
     }
-
-    /**
-     * Deletes an item from the front of Deque. Return true if the operation is successful.
-     */
+    
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
     public boolean deleteFront() {
-        if (isEmpty()) {
+        if (size == 0) {
             return false;
         }
-        // front 被设计在数组的开头，所以是 +1
-        front = (front + 1) % capacity;
-        return true;
-    }
 
-    /**
-     * Deletes an item from the rear of Deque. Return true if the operation is successful.
-     */
+        front = front == arr.length - 1 ? 0 : front + 1;
+        size--;
+        return true; 
+    }
+    
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
     public boolean deleteLast() {
-        if (isEmpty()) {
+        if (size == 0) {
             return false;
         }
-        // rear 被设计在数组的末尾，所以是 -1
-        rear = (rear - 1 + capacity) % capacity;
+
+        if (last == 0) {
+            last = arr.length - 1;
+
+        }else {
+            last --;
+        }
+        size --;
         return true;
     }
-
-    /**
-     * Get the front item from the deque.
-     */
+    
+    /** Get the front item from the deque. */
     public int getFront() {
-        if (isEmpty()) {
+        if (size == 0) {
             return -1;
         }
         return arr[front];
     }
-
-    /**
-     * Get the last item from the deque.
-     */
+    
+    /** Get the last item from the deque. */
     public int getRear() {
-        if (isEmpty()) {
+        if (size == 0) {
             return -1;
         }
-        // 当 rear 为 0 时防止数组越界
-        return arr[(rear - 1 + capacity) % capacity];
+        return last == 0 ? arr[arr.length - 1] : arr[last - 1];
     }
-
-    /**
-     * Checks whether the circular deque is empty or not.
-     */
+    
+    /** Checks whether the circular deque is empty or not. */
     public boolean isEmpty() {
-        return front == rear;
+        return size == 0;
     }
-
-    /**
-     * Checks whether the circular deque is full or not.
-     */
+    
+    /** Checks whether the circular deque is full or not. */
     public boolean isFull() {
-        // 注意：这个设计是非常经典的做法
-        return (rear + 1) % capacity == front;
+        return size == arr.length;
     }
 }
 
