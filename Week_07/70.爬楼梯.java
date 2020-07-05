cb2304e9e76c806dfe19e2bd2d30963a1d4bd78e
Admin <binyuw96@gmail.com>
2020-07-05 02:59:09 -0400

    climbing-stairs

---
+++ Week_07/70.爬楼梯.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=java
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int [] dp = new int [n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; ++i) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n];
+    }
+}
+// @lc code=end
+
